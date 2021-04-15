d0b9aae5ad6155d1b48e3d8064f5b03ffd134448
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-04-15 14:42:45 -0400

    Testing changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          From Joma's Branch
-        </p>
+        <p>Rebecca's changes</p>
         <a
           className="App-link"
           href="https://reactjs.org"
