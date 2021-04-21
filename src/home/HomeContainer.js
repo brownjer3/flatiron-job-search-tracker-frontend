122aac918a2a598c9efad5fa9aa2ef409d42729c
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-04-21 12:41:17 -0400

    Add styling to commit-blog-container, git-hubcommits, and blog-posts

--- src/home/HomeContainer.js
+++ src/home/HomeContainer.js
@@ -7,8 +7,26 @@ export default class HomeContainer extends Component {
       <div id="home-container">
         <div id="week-of">
           <h2>Week of [Insert Dates here]:</h2>
-          <i className="far fa-check-circle"></i>
-          <i className="far fa-times-circle"></i>
+          <i className="far fa-check-circle green"></i>
+          <i className="far fa-times-circle red"></i>
+        </div>
+        <div id="commit-blog-container">
+          <div id="github-commits" className="medium-box">
+            <div className="box-header">
+              <h3>Github Commits</h3>
+              <i className="far fa-check-circle light-green"></i>
+              <i className="far fa-times-circle light-red"></i>
+            </div>
+            Commits
+          </div>
+          <div id="blog-posts" className="medium-box">
+            <div className="box-header">
+              <h3>Blog Post</h3>
+              <i className="far fa-check-circle light-green"></i>
+              <i className="far fa-times-circle light-red"></i>
+            </div>
+            Posts
+          </div>
         </div>
       </div>
     );
