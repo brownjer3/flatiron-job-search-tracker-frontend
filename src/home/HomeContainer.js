565f7372a65cc5068060edfc3075d3e07318c876
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-04-21 11:11:37 -0400

    Install react-router-dom and layout HomeContainer

---
+++ src/home/HomeContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class HomeContainer extends Component {
+  render() {
+    return (
+      <div id="home-container">
+        <h1>Home</h1>
+      </div>
+    );
+  }
+}
