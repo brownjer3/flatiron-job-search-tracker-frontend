23b0cfc5878e325c967c76674b460b90d41719e1
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-04-21 13:30:45 -0400

    Add employer-contacts div to HomeContainer

--- src/home/HomeContainer.js
+++ src/home/HomeContainer.js
@@ -46,6 +46,40 @@ export default class HomeContainer extends Component {
             </div>
           </div>
         </div>
+
+        <div id="employer-contacts" className="large-box">
+          <div className="box-header">
+            <h3>Employer Contacts</h3>
+            <i className="far fa-check-circle light-green"></i>
+            <i className="far fa-times-circle light-red"></i>
+          </div>
+          <div className="box-content">
+            <p>Employer Contacts:</p>
+            <ol>
+              <li>
+                First contact with info
+                <button>Edit</button>
+                <button>Delete</button>
+              </li>
+              <li>
+                Next contact with info
+                <button>Edit</button>
+                <button>Delete</button>
+              </li>
+              <li>
+                Another contact with info
+                <button>Edit</button>
+                <button>Delete</button>
+              </li>
+              <li>
+                Again, a contact with info
+                <button>Edit</button>
+                <button>Delete</button>
+              </li>
+            </ol>
+          </div>
+          <button>Add New Employer Contact</button>
+        </div>
       </div>
     );
   }
