e00f1ca247b0f96eed4008a4110d5bfd9eb9dc4e
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-04-21 11:43:13 -0400

    Add div#week-of to HomeContainer, import font awesome library

--- src/home/HomeContainer.js
+++ src/home/HomeContainer.js
@@ -4,7 +4,11 @@ export default class HomeContainer extends Component {
   render() {
     return (
       <div id="home-container">
-        <h1>Home</h1>
+        <div id="week-of">
+          <h2>
+            Week of [Insert Dates here]: <i class="far fa-check-circle"></i>
+          </h2>
+        </div>
       </div>
     );
   }
