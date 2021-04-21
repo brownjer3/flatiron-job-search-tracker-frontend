192966dd9fe19f0fb4dfab66b31cca0c6e5c76f1
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-04-21 11:24:21 -0400

    Add NavLink to Home in Navigation.js

--- src/header/Navigation.js
+++ src/header/Navigation.js
@@ -1,3 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 export default function Navigation() {
-  return <nav>Navigation</nav>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink exact to="/" activeClassName="active-link">
+            Home
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
