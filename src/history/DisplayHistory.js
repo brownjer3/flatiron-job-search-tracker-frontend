ad9a99289a131f42156bcec05563c02a0e3b1eea
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-04-21 14:18:53 -0400

    Set up History page with DisplayHistory

---
+++ src/history/DisplayHistory.js
@@ -0,0 +1,97 @@
+export default function DisplayHistory() {
+  return (
+    <div id="display-history">
+      <div className="small-box">
+        <div className="box-header">
+          <h3>4/5 - 4/11</h3>
+        </div>
+        <div className="box-content">
+          <p>
+            Blog Post:
+            <i className="far fa-check-circle green"></i>
+            <i className="far fa-times-circle red"></i>
+          </p>
+          <p>
+            Github Commits:
+            <i className="far fa-check-circle green"></i>
+            <i className="far fa-times-circle red"></i>
+          </p>
+          <p>
+            Employer Contacts:
+            <i className="far fa-check-circle green"></i>
+            <i className="far fa-times-circle red"></i>
+          </p>
+        </div>
+      </div>
+
+      <div className="small-box">
+        <div className="box-header">
+          <h3>3/29 - 4/4</h3>
+        </div>
+        <div className="box-content">
+          <p>
+            Blog Post:
+            <i className="far fa-check-circle green"></i>
+            <i className="far fa-times-circle red"></i>
+          </p>
+          <p>
+            Github Commits:
+            <i className="far fa-check-circle green"></i>
+            <i className="far fa-times-circle red"></i>
+          </p>
+          <p>
+            Employer Contacts:
+            <i className="far fa-check-circle green"></i>
+            <i className="far fa-times-circle red"></i>
+          </p>
+        </div>
+      </div>
+
+      <div className="small-box">
+        <div className="box-header">
+          <h3>3/22 - 3/28</h3>
+        </div>
+        <div className="box-content">
+          <p>
+            Blog Post:
+            <i className="far fa-check-circle green"></i>
+            <i className="far fa-times-circle red"></i>
+          </p>
+          <p>
+            Github Commits:
+            <i className="far fa-check-circle green"></i>
+            <i className="far fa-times-circle red"></i>
+          </p>
+          <p>
+            Employer Contacts:
+            <i className="far fa-check-circle green"></i>
+            <i className="far fa-times-circle red"></i>
+          </p>
+        </div>
+      </div>
+
+      <div className="small-box">
+        <div className="box-header">
+          <h3>3/15 - 3/21</h3>
+        </div>
+        <div className="box-content">
+          <p>
+            Blog Post:
+            <i className="far fa-check-circle green"></i>
+            <i className="far fa-times-circle red"></i>
+          </p>
+          <p>
+            Github Commits:
+            <i className="far fa-check-circle green"></i>
+            <i className="far fa-times-circle red"></i>
+          </p>
+          <p>
+            Employer Contacts:
+            <i className="far fa-check-circle green"></i>
+            <i className="far fa-times-circle red"></i>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
