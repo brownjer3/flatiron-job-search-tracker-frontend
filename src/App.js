27ab9bb60becd9f100a11a6fce0e99631d8a9a3d
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-04-21 11:03:41 -0400

    Removed extra code and added Router to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomeContainer from "./home/HomeContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Rebecca's changes</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App" id="app-container">
+          {/* <HeaderContainer /> */}
+          <Switch>
+            {/* <Route path="">
+              <Container />
+            </Route>
+            <Route path="">
+              <Container />
+            </Route>
+            <Route path="">
+              <Container />
+            </Route> */}
+            <Route path="/">
+              <HomeContainer />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
