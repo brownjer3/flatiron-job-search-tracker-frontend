70aefa1e9d93117e344e08e09a1cfbda1e6240f5
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-05-04 15:45:22 -0400

    Create login form

--- src/login/LoginContainer.js
+++ src/login/LoginContainer.js
@@ -5,7 +5,34 @@ export default class LoginContainer extends Component {
   render() {
     return (
       <div id="login-container">
-        <h2>Login Page</h2>
+        <h2>Welcome Back</h2>
+        <div className="medium-box">
+          <div className="box-header">
+            <h3>Login</h3>
+          </div>
+          <form action="/login" method="post" id="login-form">
+            <p>
+              <label htmlFor="email">Email: </label>
+              <br />
+              <input
+                type="email"
+                name="email"
+                placeholder="susie@company.com"
+              />
+            </p>
+            <p>
+              <label htmlFor="password">Password: </label>
+              <br />
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter your password"
+              />
+            </p>
+
+            <input className="user-form-submit" type="submit" />
+          </form>
+        </div>
       </div>
     );
   }
