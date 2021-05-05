710e18f472e2ee14ef2987b6fa99b38ff70562b7
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-05-05 18:49:10 -0400

    Change light classes to dark

--- src/home/EmployerContacts.js
+++ src/home/EmployerContacts.js
@@ -3,8 +3,8 @@ export default function EmployerContacts() {
     <div id="employer-contacts" className="large-box">
       <div className="box-header">
         <h3>Job Search Activity</h3>
-        <i className="far fa-check-circle light-green"></i>
-        <i className="far fa-times-circle light-red"></i>
+        <i className="far fa-check-circle dark-green"></i>
+        <i className="far fa-times-circle dark-red"></i>
       </div>
       <div className="box-content">
         <p>Job Search Activity:</p>
