c9349cbff39591465dbf72722523cea3b44a7d61
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-05-05 19:24:54 -0400

    Change LoginForm to class component

--- src/login/LoginForm.js
+++ src/login/LoginForm.js
@@ -1,22 +1,26 @@
-export default function LoginForm() {
-  return (
-    <form action="/login" method="post" id="login-form">
-      <p>
-        <label htmlFor="email">Email: </label>
-        <br />
-        <input type="email" name="email" placeholder="susie@company.com" />
-      </p>
-      <p>
-        <label htmlFor="password">Password: </label>
-        <br />
-        <input
-          type="password"
-          name="password"
-          placeholder="Enter your password"
-        />
-      </p>
+import React, { Component } from "react";
 
-      <input className="user-form-submit" type="submit" />
-    </form>
-  );
+export default class LoginForm extends Component {
+  render() {
+    return (
+      <form action="/login" method="post" id="login-form">
+        <p>
+          <label htmlFor="email">Email: </label>
+          <br />
+          <input type="email" name="email" placeholder="susie@company.com" />
+        </p>
+        <p>
+          <label htmlFor="password">Password: </label>
+          <br />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+          />
+        </p>
+
+        <input className="user-form-submit" type="submit" />
+      </form>
+    );
+  }
 }
