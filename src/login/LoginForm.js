89b4c7da201ec4feff78e9fc69080118a4d60a9e
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-05-05 19:29:52 -0400

    Set up controlled form for LoginForm

--- src/login/LoginForm.js
+++ src/login/LoginForm.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 
 export default class LoginForm extends Component {
+  state = {
+    email: "",
+    password: "",
+  };
+
+  handleChange = (e) => {
+    console.log(e.target.value);
+    let value = e.target.value;
+    this.setState({ ...this.state, [e.target.name]: value });
+  };
+
   render() {
     return (
       <form action="/login" method="post" id="login-form">
         <p>
           <label htmlFor="email">Email: </label>
           <br />
-          <input type="email" name="email" placeholder="susie@company.com" />
+          <input
+            type="email"
+            name="email"
+            placeholder="susie@company.com"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
         </p>
         <p>
           <label htmlFor="password">Password: </label>
@@ -16,6 +33,8 @@ export default class LoginForm extends Component {
             type="password"
             name="password"
             placeholder="Enter your password"
+            value={this.state.password}
+            onChange={this.handleChange}
           />
         </p>
 
