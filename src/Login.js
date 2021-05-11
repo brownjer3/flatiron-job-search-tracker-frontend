45f14f6647693a48ddfa8654b72927fbb93d91f3
jomapormentilla <jomapormentilla@gmail.com>
2021-05-11 10:55:10 -0400

    Update stylings.

--- src/Login.js
+++ src/Login.js
@@ -3,9 +3,10 @@ import React from 'react'
 const style = {
     login: {
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        height: '100vh'
+        height: '90vh'
     },
     form: {
         border: 'none'
@@ -25,15 +26,16 @@ class Login extends React.Component {
     }
 
     handleOnSubmit = e => {
-
+        
     }
 
     render(){
         return(
             <div className="login" style={ style.login }>
+                <h3>Flatiron Job Search Tracker</h3><br />
                 <form style={ style.form } onSubmit={ this.handleOnSubmit }>
                     <input type="text" name="email" placeholder="Email" onChange={ this.handleOnChange } /><br />
-                    <input type="text" name="password" placeholder="Password" onChange={ this.handleOnChange } />
+                    <input type="text" name="password" placeholder="Password" onChange={ this.handleOnChange } /><br />
                     <button type="submit">Login</button>
                 </form>
             </div>
