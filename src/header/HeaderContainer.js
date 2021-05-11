10f1f4b594d1f36fb73b17389ea59dc8c6a0329a
jomapormentilla <jomapormentilla@gmail.com>
2021-05-11 14:18:54 -0400

    Update Logo in HeaderContainer. Fix UL indentation in Navigation.

--- src/header/HeaderContainer.js
+++ src/header/HeaderContainer.js
@@ -4,7 +4,8 @@ import "./header.css";
 export default function HeaderContainer() {
   return (
     <div id="header-container">
-      <h1 id="header-text">Flatiron Job Search Tracker</h1>
+      {/* <h1 id="header-text">Flatiron Job Search Tracker</h1> */}
+      <img src="/images/flatiron-jst.png" alt="Logo" style={{ maxWidth: '300px' }} /><br />
       <Navigation />
     </div>
   );
